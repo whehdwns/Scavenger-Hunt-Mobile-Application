b79bd5de6c50dc4d146bdc8e7d85c38234d3acc2
Chi Hong Kou <henrykou2@gmail.com>
2019-03-17 20:20:20 -0700

    added xml for create room

---
+++ app/src/main/java/com/example/login/createRoom.java
@@ -0,0 +1,13 @@
+package com.example.login;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class createRoom extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_room);
+    }
+}
