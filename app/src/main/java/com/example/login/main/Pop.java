de94276558ac90bc47781a2210048f76019727e0
Chi Hong Kou <henrykou2@gmail.com>
2019-04-14 15:06:55 -0700

    fixed popup

--- app/src/main/java/com/example/login/main/Pop.java
+++ app/src/main/java/com/example/login/main/Pop.java
@@ -3,13 +3,21 @@ package com.example.login.main;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
+import android.view.Gravity;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.example.login.R;
 
 public class Pop extends AppCompatActivity {
-
+        EditText editTextDescription;
+        Button buttonCreateTask;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        EditText editTextDescription = (EditText) findViewById(R.id.editTextDescription);
+        Button buttonCreateTask = (Button) findViewById(R.id.buttonCreateTask);
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pop);
         DisplayMetrics dm = new DisplayMetrics();
@@ -19,5 +27,11 @@ public class Pop extends AppCompatActivity {
         int height = dm.heightPixels;
 
         getWindow().setLayout((int)(width*.8),(int)(height*.6));
+
+        WindowManager.LayoutParams params =  getWindow().getAttributes();
+        params.gravity = Gravity.CENTER;
+        params.x = 0;
+        params.y = 60;
+        getWindow().setAttributes(params);
     }
 }
