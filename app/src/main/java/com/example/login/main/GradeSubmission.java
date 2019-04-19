de388b2b7a3c293165a920893f49368fdabb5343
Chi Hong Kou <henrykou2@gmail.com>
2019-04-18 20:13:59 -0700

    added XMLs

---
+++ app/src/main/java/com/example/login/main/GradeSubmission.java
@@ -0,0 +1,15 @@
+package com.example.login.main;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.login.R;
+
+public class GradeSubmission extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_grade_submission);
+    }
+}
